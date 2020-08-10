2cf98073bdb01f530b6a4113e4a6d384b0eb03d1
Charlie Wallin <charlie@charliewallin.com>
2020-08-10 16:08:07 -0400

    Completed 02-05-refer-to-an-instance

--- class_methods.php
+++ class_methods.php
@@ -9,7 +9,13 @@ class Student {
   function say_hello() {
     return "Hello";
   }
+
+  function full_name() {
+    return $this->first_name . " " . $this->last_name . "<br>";
+  }
 }
+
+
 
 $student1 = new Student;
 $student1->first_name = 'Lucy';
@@ -19,11 +25,8 @@ $student2 = new Student;
 $student2->first_name = 'Ethel';
 $student2->last_name = 'Mertz';
 
-echo $student1->first_name . " " . $student1->last_name . '<br>';
-echo $student2->first_name . " " . $student2->last_name . '<br>';
-
-echo $student1->say_hello();
-echo $student2->say_hello();
+echo $student1->full_name() . '<br>';
+echo $student2->full_name() . '<br>';
 
 // class methods example
 
